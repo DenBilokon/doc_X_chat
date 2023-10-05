223b71fffe867561ef90be1ed84063e12e2b47eb
Sanyavas <vasilinaoleksandr@gmail.com>
2023-10-05 22:57:04 +0300

    add user

--- doc_X_chat/doc_X_chat/urls.py
+++ doc_X_chat/doc_X_chat/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('chat.urls')),
-    # path('users/', include('user.urls')),
+    # path('user/', include('user.urls')),
     # path('llm_ai/', include('llm_ai.urls')),
     # path('chat/', include('chat.urls')),
 ]
--- doc_X_chat/user/models.py
+++ doc_X_chat/user/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 
-# Create your models here.
-class CustomProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=15)
-    address = models.CharField(max_length=100)
+# # Create your models here.
+# class CustomProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     phone = models.CharField(max_length=15)
+#     address = models.CharField(max_length=100)
+#
+#
+#     def __str__(self):
+#         return self.user.username
 
 
-    def __str__(self):
-        return self.user.username
+EXTENSIONS_IMG = ['jpeg', 'png', 'jpg', 'svg', 'gif']
+
+
+class Avatar(models.Model):
+    image = CloudinaryField(resource_type='auto', allowed_formats=EXTENSIONS_IMG, folder='avatars doc_X_chat')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1, null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
